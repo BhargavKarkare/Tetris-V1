9754fa3ed3fabeffa030a859b44f125c9b6897d2
Bhargav <karkarebhargav08@gmail.com>
2024-05-09 10:54:56 +0530

    backend.tf modified

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bhargavbuckettf " # Replace with your actual S3 bucket name
+    bucket = "eks-bucket-tfstate" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
